9827801b6eb1e374694a6361fac25ad9808a9893
LauraTkalcec <tkalcec.laura@gmail.com>
2019-05-12 15:44:45 +0200

    popis svih procesa

--- Popis svih procesa/Program.cs
+++ Popis svih procesa/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Popis_svih_procesa
 {
@@ -10,6 +11,18 @@ namespace Popis_svih_procesa
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n   Popis  svih procesa:    \n");
+            Process[] procesi = Process.GetProcesses();
+            foreach (Process p in procesi)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("identifikator:{0}", p.Id);
+                Console.WriteLine("Naziv procesa:{0}", p.ProcessName);
+                Console.WriteLine("Proces radi:{0}", p.Responding);
+                Console.WriteLine("Broj dretvi:{0}", p.Threads.Count);
+                Console.WriteLine("Kolicina radne memorije: {0}  MB", p.WorkingSet64);
+            }
+
         }
     }
 }
